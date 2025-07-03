b886441870df62e19e8739460e3481772459c43a
נור <njuidatnor750@gmail.com>
2025-07-03 14:40:08 +0300

    Update final.ino
    הגדרת השרת  ו גם הנתיב  ו הגדרת של הפינים ש מתחברים  ו גם משתנה bool עבור ה מצוקה

--- final.ino
+++ final.ino
@@ -1,3 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+const char* websocket_server = "10.0.0.14";
+const int websocket_port = 3000;
+const char* websocket_path = "/";
+
+const int buttonPin = D1;
+const int ledPin = D2;
+const int buzzerPin = D3;
+
+volatile bool panicPressed = false;
