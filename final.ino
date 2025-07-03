cf3351be6c529158e36d3b84fe091c2b1041b729
נור <njuidatnor750@gmail.com>
2025-07-03 14:48:38 +0300

    Update final.ino
    מגדירים מצבים אפשריים למערכת ומשייכים את המצב ההתחלתי לשגרה
    פונקציה שמייצרת הודעת JSON עם מזהה המכשיר והסטטוס הנוכחי
    משתנים לניהול הבהוב LED ומצבו הנוכחי, ואובייקט WebSocket לתקשורת עם השרת

--- final.ino
+++ final.ino
@@ -15,3 +15,26 @@ const int ledPin = D2;
 const int buzzerPin = D3;
 
 volatile bool panicPressed = false;
+
+
+enum State { NORMAL, ALERT, NEUTRALIZED };
+State systemState = NORMAL;
+
+unsigned long lastBlink = 0;
+bool ledState = false;
+
+WebSocketsClient webSocket;
+
+void IRAM_ATTR handleInterrupt() {
+  panicPressed = true;
+}
+
+void sendStatus(const char* status) {
+  StaticJsonDocument<128> doc;
+  doc["device_id"] = "ESP01";
+  doc["status"] = status;
+  
+  String jsonStr;
+  serializeJson(doc, jsonStr);
+  webSocket.sendTXT(jsonStr);
+}
