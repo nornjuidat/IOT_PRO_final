15d2977f78e342d9cdb93c7465265f17ba92e0eb
נור <njuidatnor750@gmail.com>
2025-07-04 16:00:27 +0300

    Update server.js
    זיהוי חיבור שמגיע מה־ESP ושמירתו למעקב עתידי
    שליחת הודעות מה־ESP לדפדפן כאשר הוא מחובר – תיווך תקשורת בזמן אמת.

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const wss = new WebSocket.Server({ port: PORT });
 
 let espSocket = null;
 let browserSocket = null;
+
+
+wss.on("connection", (ws, req) => {
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    try {
+      const data = JSON.parse(message);
+
+      if (data.device_id) {
+        console.log("Message from ESP:", data);
+        espSocket = ws;
+
+        if (browserSocket && browserSocket.readyState === WebSocket.OPEN) {
+          browserSocket.send(JSON.stringify(data));
+        }
+      }
